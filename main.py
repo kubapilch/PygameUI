430c236bf2d6be615c20e76fbde1c6dd407f56fc
Jakub Pilch <kuba.pilch2001@gmail.com>
2019-11-10 14:53:54 +0100

    Improved Sliders and overall improvments

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import pygame
+from decimal import Decimal
 
 class UIObject():
-    def __init__(self, placement:tuple, color, alpha):
+    def __init__(self, placement:tuple):
         self.placement = placement # (x, y, width, height)
         self.x = placement[0]
         self.y = placement[1]
         self.width = placement[2]
         self.height = placement[3]
-        self._color = color
-        self._alpha = alpha
-        self.__sub_objects = []
 
-        self.createSurface()
+        self.__sub_objects = []
 
     @property
     def size(self):
@@ -19,7 +17,22 @@ class UIObject():
 
     @property
     def position(self):
-        return (self.placement[0], self.placement[1])
+        return (self.placement[0], self.placement[1])   
+
+    def draw(self, surface):
+        """
+        Draw all sub objects, their sub objects and main object to a given surface
+        """
+        pass
+
+class Surfaces(UIObject):
+    def __init__(self, placement:tuple, color, alpha):
+        super().__init__(placement)
+        self._color = color
+        self._alpha = alpha
+        self.__sub_objects = []
+
+        self.createSurface()
 
     @property
     def color(self):
@@ -47,7 +60,7 @@ class UIObject():
         self.surface = pygame.Surface(self.size, pygame.SRCALPHA)
 
         # Set color and alpha
-        self.surface.fill((*self.color, self.alpha))
+        self.surface.fill((*self._color, self._alpha))
 
     def add_sub_object(self, obj):
         """
@@ -56,22 +69,12 @@ class UIObject():
         self.__sub_objects.append(obj)
 
     def delete_sub_object(self, obj):
-        self.__sub_objects.remove(obj)     
+        self.__sub_objects.remove(obj)  
 
-    def draw(self, surface):
-        """
-        Draw all sub objects, their sub objects and main object to a given surface
-        """
-        # Create surface for subobjects
-        # Draw subobjects and their subobjects
-        # Blit surface to given surface
-        pass
-
-
-
-class Placeholder(UIObject):
+class Placeholder(Surfaces):
 
     def __init__(self, placement:tuple):
+        super().__init__(placement, (0, 0, 0), 0)
         self.placement = placement # (x, y, width, height)
         self.x = placement[0]
         self.y = placement[1]
@@ -121,7 +124,7 @@ class Placeholder(UIObject):
     def alpha(self, a):
         pass
 
-class Background(UIObject):
+class Background(Surfaces):
     def __init__(self, placement:tuple, color, alpha):
         super().__init__(placement, color, alpha)
         self.__sub_objects = []
@@ -149,14 +152,15 @@ class Background(UIObject):
 
 
 class Button(UIObject):
-    def __init__(self, placement:tuple, parent_position:tuple, color, alpha, text, font="monospace", font_size=10, font_color=(0, 0, 0), click_function=None):
-        super().__init__(placement, color, alpha)
+    def __init__(self, placement:tuple, reference_position:tuple, color, alpha, text, font="monospace", font_size=10, font_color=(0, 0, 0), click_function=None):
+        super().__init__(placement)
         self.font = font
         self.font_size = font_size
         self.font_color = font_color
-        self.text = text
         self.color = color
-        self.parent_position = parent_position
+        self.alpha = alpha
+        self.reference_position = reference_position
+        self.text = text
         self.click_function = click_function
 
     @property
@@ -174,10 +178,10 @@ class Button(UIObject):
         pygame.draw.rect(surface, (*self.color, self.alpha), self.placement)
         # Draw label
         surface.blit(self.label, (self.x + (self.width/2 - self.label.get_width()/2), self.y+(self.height/2 - self.label.get_height()/2)))
-    
+
     def clicked(self, pos):
-        if pos[0] > self.x + self.parent_position[0] and pos[0] < self.x + self.width + self.parent_position[0]:
-            if pos[1] > self.y + self.parent_position[1]  and pos[1] < self.y + self.height + self.parent_position[1] :
+        if pos[0] > self.x + self.reference_position[0] and pos[0] < self.x + self.width + self.reference_position[0]:
+            if pos[1] > self.y + self.reference_position[1]  and pos[1] < self.y + self.height + self.reference_position[1] :
                 # If user speciefied small function execute it
                 if self.click_function is not None:
                     self.click_function()
@@ -187,16 +191,17 @@ class Button(UIObject):
         return False
 
 class Checkbox(UIObject):
-    def __init__(self, placement:tuple, parent_position:tuple, checkbox_color, indicator_color, alpha, text, spacing=10, font="monospace", font_size=10, font_color=(0, 0, 0), click_function=None):
-        super().__init__(placement, (0, 0, 0), alpha)
+    def __init__(self, placement:tuple, reference_position:tuple, checkbox_color, indicator_color, alpha, text, spacing=10, font="monospace", font_size=10, font_color=(0, 0, 0), click_function=None):
+        super().__init__(placement)
         self.font = font
         self.font_size = font_size
         self.font_color = font_color
-        self.text = text
         self.checkbox_color = checkbox_color
         self.indicator_color = indicator_color
-        self.parent_position = parent_position
+        self.alpha = alpha
+        self.reference_position = reference_position
         self.spacing = spacing
+        self.text = text
         self.click_function = click_function
         self.checked = False
 
@@ -222,8 +227,8 @@ class Checkbox(UIObject):
         surface.blit(self.label, (self.x + self.width + 10, self.y+(self.height/2 - self.label.get_height()/2)))
     
     def clicked(self, pos):
-        if pos[0] > self.x + self.parent_position[0] and pos[0] < self.x + self.width + self.parent_position[0]:
-            if pos[1] > self.y + self.parent_position[1]  and pos[1] < self.y + self.height + self.parent_position[1] :
+        if pos[0] > self.x + self.reference_position[0] and pos[0] < self.x + self.width + self.reference_position[0]:
+            if pos[1] > self.y + self.reference_position[1]  and pos[1] < self.y + self.height + self.reference_position[1] :
                 # Unmark or mark 
                 self.checked = not self.checked
                 
@@ -236,15 +241,16 @@ class Checkbox(UIObject):
         return False
 
 class Slider(UIObject):
-    def __init__(self, placement:tuple, parent_position:tuple, min_value, max_value, jump, default_value, slider_color, bar_color, slider_radius, alpha, text, spacing=10, font="monospace", font_size=10, font_color=(0, 0, 0), click_function=None):
-        super().__init__(placement, (0, 0, 0), alpha)
+    def __init__(self, placement:tuple, reference_position:tuple, min_value, max_value, jump, default_value, slider_color, bar_color, slider_radius, alpha, text, spacing=10, font="monospace", font_size=10, font_color=(0, 0, 0), click_function=None):
+        super().__init__(placement)
         self.font = font
         self.font_size = font_size
         self.font_color = font_color
-        self.text = text
         self.slider_color = slider_color
         self.bar_color = bar_color
-        self.parent_position = parent_position
+        self.alpha = alpha
+        self.text = text
+        self.reference_position = reference_position
         self.spacing = spacing
         self.click_function = click_function
         self.min_value = min_value
@@ -281,13 +287,14 @@ class Slider(UIObject):
         surface.blit(self.label, (self.x + (self.width/2 - self.label.get_width()/2), self.y + self.slider_radius*2 + self.label.get_height()/2))
 
     def clicked(self, pos):
-        if pos[0] + self.slider_radius > self.x + self.parent_position[0] and pos[0] - self.slider_radius < self.x + self.width + self.parent_position[0]:
-            if pos[1] + self.slider_radius > self.y + self.parent_position[1]  and pos[1] - self.slider_radius  < self.y + self.height + self.parent_position[1] :
+        if pos[0] + self.slider_radius > self.x + self.reference_position[0] and pos[0] - self.slider_radius < self.x + self.width + self.reference_position[0]:
+            if pos[1] + self.slider_radius > self.y + self.reference_position[1]  and pos[1] - self.slider_radius  < self.y + self.height + self.reference_position[1] :
                 # Set new value
-                bar_x_pos = self.position[0] + self.parent_position[0]
+                bar_x_pos = self.position[0] + self.reference_position[0]
                 distance = pos[0] - bar_x_pos
                 pixels_per_one_jump = (self.placement[2]/((self.max_value - self.min_value)/self.jump))
-                new_value = round(distance/pixels_per_one_jump, 0)*self.jump + self.min_value
+                round_to = abs(Decimal(str(self.jump)).as_tuple().exponent)
+                new_value = round(round(distance/pixels_per_one_jump, round_to)*self.jump, round_to) + self.min_value
 
                 # Make sure it's not out of scale
                 if new_value > self.max_value:
